1553f57099ca1f7e2251e0fc2400aeffc19efb53
focss <focss_lin@qq.com>
2023-06-26 05:36:18 +0800

    new file:   user_feather2xlsx.py

---
+++ user_feather2xlsx.py
@@ -0,0 +1,18 @@
+# coding = UTF-8
+from pathlib import Path
+import pandas as pd
+
+
+def main() -> None:
+    input_file_path = Path('./Temp/event_chart.feather')
+    output_file_path = Path('./Temp/event_chart.xlsx')
+    dtf = pd.read_feather(input_file_path)
+    dtf.to_excel(output_file_path, index=False)
+    input_file_path = Path('./Temp/readdata_original.feather')
+    output_file_path = Path('./Temp/readdata_original.xlsx')
+    dtf = pd.read_feather(input_file_path)
+    dtf.to_excel(output_file_path, index=False)
+
+
+if __name__ == '__main__':
+    main()
